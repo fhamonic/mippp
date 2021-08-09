2480196908f7dab8a7df71bb6439d07c85ad1b78
PlaiSeek <francois.hamonic@gmail.com>
2021-08-10 01:49:54 +0200

    cmake rules + try scip

---
+++ include/milppp/solver_traits/scip_traits.hpp
@@ -0,0 +1,32 @@
+#ifndef MILPP_GUROBI_TRAITS_HPP
+#define MILPP_GUROBI_TRAITS_HPP
+
+#include "scip/scip.h"
+
+struct ScipTraits {
+    enum OptSense : int { MINIMIZE=SCIP_OBJSENSE_MINIMIZE, MAXIMIZE=SCIP_OBJSENSE_MAXIMIZE };
+    enum ColType : char { CONTINUOUS=SCIP_VARTYPE_CONTINUOUS, INTEGER=SCIP_VARTYPE_INTEGER, BINARY=SCIP_VARTYPE_BINARY };
+    using ModelType = void;
+
+    static void build(OptSense opt_sense,
+                              int nb_vars, 
+                              double *obj,
+                              double *col_lb,
+                              double *col_ub,
+                              ColType *vtype,
+                              int nb_rows,
+                              int nb_elems,
+                              int *row_begins,
+                              int *indices,
+                              double *coefs,
+                              double *row_lb,
+                              double *row_ub) {
+        
+
+        SCIP
+
+        return;
+    }
+};
+
+#endif //MILPP_GUROBI_TRAITS_HPP
