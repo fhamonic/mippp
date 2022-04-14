084d33546a0457bfd83edebc9a395e04f0c5d2b0
= <francois.hamonic@gmail.com>
2022-04-14 17:59:38 +0200

    oeprators header

---
+++ include/mippp/operators.hpp
@@ -0,0 +1,7 @@
+#ifndef MIPPP_OPERATORS_HPP
+#define MIPPP_OPERATORS_HPP
+
+#include "mippp/expressions/linear_expression_operators.hpp"
+#include "mippp/constraints/linear_constraint_operators.hpp"
+
+#endif  // MIPPP_OPERATORS_HPP
