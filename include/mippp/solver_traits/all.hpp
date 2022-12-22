2819fb32618a91d15b1e2d29909bce1f53d363bf
= <francois.hamonic@gmail.com>
2022-12-22 16:57:11 +0100

    prefered solver preprocessor

--- include/mippp/solver_traits/all.hpp
+++ include/mippp/solver_traits/all.hpp
@@ -1,31 +1,39 @@
 #ifndef MIPPP_SOLVER_TRAITS_HPP
 #define MIPPP_SOLVER_TRAITS_HPP
 
-#ifdef GUROBI_FOUND
+#ifdef MIPPP_GUROBI_FOUND
 #include "grb_traits.hpp"
+#define MIPPP_GUROBI 1
 #endif
-#ifdef CPLEX_FOUND
+#ifdef MIPPP_CPLEX_FOUND
 #include "cplex_traits.hpp"
+#define MIPPP_CPLEX 2
 #endif
-#ifdef SCIP_FOUND
+#ifdef MIPPP_SCIP_FOUND
 #include "scip_traits.hpp"
+#define MIPPP_SCIP 3
 #endif
-#ifdef COIN_FOUND
+#ifdef MIPPP_COIN_FOUND
 #include "cbc_traits.hpp"
+#define MIPPP_COIN 4
 #endif
 
 namespace fhamonic {
 namespace mippp {
-#if defined(GUROBI_FOUND)
+#if defined(MIPPP_GUROBI_FOUND) && \
+    (!defined(MIPPP_PREFERED_SOLVER) || MIPPP_PREFERED_SOLVER == MIPPP_GUROBI)
 using default_solver_traits = grb_traits;
-#elif defined(CPLEX_FOUND)
+#elif defined(MIPPP_CPLEX_FOUND) && \
+    (!defined(MIPPP_PREFERED_SOLVER) || MIPPP_PREFERED_SOLVER == MIPPP_CPLEX)
 using default_solver_traits = cplex_traits;
-#elif defined(SCIP_FOUND)
+#elif defined(MIPPP_SCIP_FOUND) && \
+    (!defined(MIPPP_PREFERED_SOLVER) || MIPPP_PREFERED_SOLVER == MIPPP_SCIP)
 using default_solver_traits = scip_traits;
-#elif defined(COIN_FOUND)
+#elif defined(MIPPP_COIN_FOUND) && \
+    (!defined(MIPPP_PREFERED_SOLVER) || MIPPP_PREFERED_SOLVER == MIPPP_COIN)
 using default_solver_traits = cbc_traits;
 #else
-static_assert(false, "No MIP solver has been found.");
+static_assert(false, "No thirdparty MIP solver has been found.");
 #endif
 }  // namespace mippp
 }  // namespace fhamonic
