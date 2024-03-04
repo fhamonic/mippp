3010eaaf3b072973a924f60895a83c740b204496
plaiseek <francois.hamonic@gmail.com>
2024-03-04 15:35:15 +0100

    is_cli_available + skip tests if not

--- test/cli_grb_test.cpp
+++ test/cli_grb_test.cpp
@@ -3,6 +3,7 @@
 #include "mippp/constraints/linear_constraint_operators.hpp"
 #include "mippp/expressions/linear_expression_operators.hpp"
 #include "mippp/mip_model.hpp"
+#include "mippp/solver_traits/is_cli_available.hpp"
 #include "mippp/xsum.hpp"
 
 #include "assert_eq_ranges.hpp"
@@ -15,41 +16,59 @@ using Var = variable<int, double>;
 using M = mip_model<cli_grb_traits>;
 
 GTEST_TEST(cli_grb_model, default_constructor) {
+    if(!is_grb_available()) GTEST_SKIP();
     model_tests<M>::default_constructor();
 }
 GTEST_TEST(cli_grb_model, add_variable_default_name_default_options) {
+    if(!is_grb_available()) GTEST_SKIP();
     model_tests<M>::add_variable_default_name_default_options();
 }
 GTEST_TEST(cli_grb_model, add_variable_default_name_custom_options) {
+    if(!is_grb_available()) GTEST_SKIP();
     model_tests<M>::add_variable_default_name_custom_options();
 }
 GTEST_TEST(cli_grb_model, add_variable_custom_name_default_options) {
+    if(!is_grb_available()) GTEST_SKIP();
     model_tests<M>::add_variable_custom_name_default_options();
 }
 GTEST_TEST(cli_grb_model, add_variable_custom_name_custom_options) {
+    if(!is_grb_available()) GTEST_SKIP();
     model_tests<M>::add_variable_custom_name_custom_options();
 }
 GTEST_TEST(cli_grb_model, add_variables_default_name_default_options) {
+    if(!is_grb_available()) GTEST_SKIP();
     model_tests<M>::add_variables_default_name_default_options();
 }
 GTEST_TEST(cli_grb_model, add_variables_default_name_custom_options) {
+    if(!is_grb_available()) GTEST_SKIP();
     model_tests<M>::add_variables_default_name_custom_options();
 }
 GTEST_TEST(cli_grb_model, add_variables_custom_name_default_options) {
+    if(!is_grb_available()) GTEST_SKIP();
     model_tests<M>::add_variables_custom_name_default_options();
 }
 GTEST_TEST(cli_grb_model, add_variables_custom_name_custom_options) {
+    if(!is_grb_available()) GTEST_SKIP();
     model_tests<M>::add_variables_custom_name_custom_options();
 }
 GTEST_TEST(cli_grb_model, add_to_objective) {
+    if(!is_grb_available()) GTEST_SKIP();
     model_tests<M>::add_to_objective();
 }
-GTEST_TEST(cli_grb_model, get_objective) { model_tests<M>::get_objective(); }
+GTEST_TEST(cli_grb_model, get_objective) {
+    if(!is_grb_available()) GTEST_SKIP();
+    model_tests<M>::get_objective();
+}
 
 GTEST_TEST(cli_grb_model, add_and_get_constraint) {
+    if(!is_grb_available()) GTEST_SKIP();
     model_tests<M>::add_and_get_constraint();
 }
-GTEST_TEST(cli_grb_model, build_optimize) { model_tests<M>::build_optimize(); }
+GTEST_TEST(cli_grb_model, build_optimize) {
+    if(!is_grb_available()) GTEST_SKIP();
+    model_tests<M>::build_optimize();
+}
 GTEST_TEST(cli_grb_model, add_to_objective_xsum) {
+    if(!is_grb_available()) GTEST_SKIP();
     model_tests<M>::add_to_objective_xsum();
 }
